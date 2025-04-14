ffb26f1bc82acab4d164e1995d1d8cbb00901d0d
Karimtabikh <karim.tabikh@Outlook.com>
2025-04-14 12:53:14 +0300

    Files names lower case

--- lib/project-data.ts
+++ lib/project-data.ts
@@ -68,9 +68,9 @@ export interface ProjectDetail {
       },
       // Changed from object to array
       images: [
-        "/projects/Wise/Insights.png",
-        "/projects/Wise/database.png",
-        "/projects/Wise/Interview.png"
+        "/projects/wise/Insights.png",
+        "/projects/wise/database.png",
+        "/projects/wise/Interview.png"
       ]
     },
     {
@@ -108,7 +108,7 @@ export interface ProjectDetail {
           clientSatisfaction: "The Deputy expressed interest in expanding the system to manage more public-facing initiatives."
         },
         images: [
-          "/projects/Kabbara/showcase.png"
+          "/projects/kabbara/showcase.png"
         ]
       },
       {
@@ -146,7 +146,7 @@ export interface ProjectDetail {
           clientSatisfaction: "CodingCLebanon praised the redesign and reported increased engagement across the platform."
         },
         images: [
-          "/projects/Codingclebanon/showcase.png"
+          "/projects/codingclebanon/showcase.png"
         ]
       },
       {
